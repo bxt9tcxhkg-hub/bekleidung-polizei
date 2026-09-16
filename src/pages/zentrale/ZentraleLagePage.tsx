3430a18d273c962a99fee33f72ace8323ac2032d
bxt9tcxhkg-hub <bxt9tcxhkg@privaterelay.appleid.com>
2026-09-16 14:29:51 +0200

    Operative Lage auf Einsätze umleiten

--- src/pages/zentrale/ZentraleLagePage.tsx
+++ src/pages/zentrale/ZentraleLagePage.tsx
@@ -1,18 +1,5 @@
-import { useOutletContext } from 'react-router-dom'
-import { EntryList } from '../../components/ZentraleEntryEditor'
-import type { ZentraleContext } from './ZentraleShell'
+import { Navigate } from 'react-router-dom'
 
 export default function ZentraleLagePage() {
-  const ctx = useOutletContext<ZentraleContext>()
-  return <EntryList
-    title="Operative Lage"
-    description='Ereignisse, Sperren, Gefahren- und Lagehinweise – wird aus einem Einsatz auf der Seite „Einsätze" erklärt.'
-    entries={ctx.lageEntries}
-    canManage={ctx.canOperateZentrale}
-    // Entsteht ausschließlich über die Aktion an einem Einsatz - kein eigenständiges "Neu" hier.
-    openNew={() => {}}
-    openEdit={ctx.openEditEntry}
-    incidentsById={ctx.incidentsById}
-    hideCreate
-  />
+  return <Navigate to="/zentrale/einsaetze" replace />
 }
