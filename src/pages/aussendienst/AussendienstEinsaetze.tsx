3f84bb7916d99ea5903400e20242c9be005478cc
bxt9tcxhkg-hub <bxt9tcxhkg@privaterelay.appleid.com>
2026-09-22 22:48:01 +0200

    feat: show compact incident safety indicator

--- src/pages/aussendienst/AussendienstEinsaetze.tsx
+++ src/pages/aussendienst/AussendienstEinsaetze.tsx
@@ -4,5 +4,5 @@ import { EntryOrIncidentList } from './aussendienstShared'
 
 export default function AussendienstEinsaetze() {
   const ctx = useOutletContext<AussendienstContext>()
-  return <EntryOrIncidentList kind="incidents" incidents={ctx.incidents} baustellen={ctx.baustellen} ownVehicleId={ctx.ownVehicle?.id ?? null} incidentSupports={ctx.incidentSupports} takeOverIncident={ctx.takeOverIncident} releaseIncidentTakeover={ctx.releaseIncidentTakeover} supportIncident={ctx.supportIncident} stopSupportingIncident={ctx.stopSupportingIncident} completeIncident={ctx.completeIncident} reopenIncident={ctx.reopenIncident} />
+  return <EntryOrIncidentList kind="incidents" incidents={ctx.incidents} baustellen={ctx.baustellen} ownVehicleId={ctx.ownVehicle?.id ?? null} incidentSupports={ctx.incidentSupports} takeOverIncident={ctx.takeOverIncident} releaseIncidentTakeover={ctx.releaseIncidentTakeover} supportIncident={ctx.supportIncident} stopSupportingIncident={ctx.stopSupportingIncident} completeIncident={ctx.completeIncident} reopenIncident={ctx.reopenIncident} incidentContextSummary={ctx.incidentContextSummary} />
 }
