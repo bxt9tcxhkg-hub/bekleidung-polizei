dc4465eb5050352525e7a3e994b57a07a3d5a7a3
bxt9tcxhkg-hub <bxt9tcxhkg@privaterelay.appleid.com>
2026-09-23 07:12:49 +0200

    chore: remove central party editing props

--- src/pages/zentrale/ZentraleUebersicht.tsx
+++ src/pages/zentrale/ZentraleUebersicht.tsx
@@ -157,8 +157,6 @@ export default function ZentraleUebersicht() {
       close={() => setWorkIncident(null)}
       openEditIncident={ctx.openEditIncident}
       completeIncident={ctx.completeIncident}
-      persons={ctx.persons}
-      onPersonCreated={ctx.onPersonCreated}
       createdBy={ctx.createdBy}
     /> : null}
   </div>
