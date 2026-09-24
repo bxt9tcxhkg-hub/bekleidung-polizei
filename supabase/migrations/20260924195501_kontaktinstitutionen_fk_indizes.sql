2ca787aea4b8e437df1cdf4b3861fb30ecfefd52
bxt9tcxhkg-hub <bxt9tcxhkg@privaterelay.appleid.com>
2026-09-24 21:55:48 +0200

    Kontaktinstitutionen indizieren

---
+++ supabase/migrations/20260924195501_kontaktinstitutionen_fk_indizes.sql
@@ -0,0 +1,7 @@
+create index zentrale_kontakt_institutionen_created_by_idx
+  on public.zentrale_kontakt_institutionen (created_by)
+  where created_by is not null;
+
+create index zentrale_kontakte_institution_idx
+  on public.zentrale_kontakte (institution)
+  where institution is not null;
